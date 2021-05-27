4ad653ed7d1204f5e23b92aaa23f5ca7f8e81698
Nuno Mata <nunomata619@gmail.com>
2021-05-27 23:11:24 +0100

    testing widget

--- widgetweather-server/src/app.js
+++ widgetweather-server/src/app.js
@@ -4,7 +4,13 @@ const express = require('express')
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
 
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index')
+})
+
 
 app.get('', (req, res) => {
     res.send('<h1>Weather</h1>')
